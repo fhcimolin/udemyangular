8969c0c2ebfed9dc2c509c5d47af3cd2d25f2d88
DEPSSC\flavio.simolin <flavio@deps.com.br>
2022-04-26 12:17:20 -0300

    Practising components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ɵInternalFormsSharedModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SuccessAlert } from './success-alert/success-alert.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SuccessAlert,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlertComponent,
   ],
   imports: [
     BrowserModule,
