1f1f7c4ba1d76dd9881e71591e70d994a665e48d
DEPSSC\flavio.simolin <flavio@deps.com.br>
2022-05-10 12:14:56 -0300

    Directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created'
   serverName = 'testServer';
+  serverCreated = false;
+  servers = ['Testserver','Testserver2'];
 
   constructor() {
-    
   }
 
   ngOnInit(): void {
@@ -18,6 +19,8 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverName = '';
   }
 
